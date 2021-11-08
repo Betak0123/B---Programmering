cc147ad58de735b1c9ce2985830b46e0e406db8a
Betak0123 <Bjarke2003@gmail.com>
2021-11-08 18:33:53 +0100

    API - HJEMMEARBEJDE

--- 14 - REST - API/index.js
+++ 14 - REST - API/index.js
@@ -1,21 +1,32 @@
-
+let loader = document.querySelector('.loader')
 
 const getActivity = () => {
-    fetch('https://www.boredapi.com/api/activity')
+    document.querySelector('#title').innerHTML = ''
+    document.querySelector('#participants').innerHTML = ''
+    document.querySelector('#acc').innerHTML = ''
+    document.querySelector('#price').innerHTML = ''
+    document.querySelector('#cat').innerHTML =''
+    loader.style.visibility = 'visible' 
+    
+    fetch('https://www.boredapi.com/api/activity'
+    +document.querySelector('#pet-select').value
+    )
     // Når man kalder noget på nettet, får vi et response ojbekt.
         .then(response => response.json())
         .then(json => {
+            console.log(document.querySelector('#pet-select').value)
             console.log(json)
             createCard(json)
+            loader.style.visibility = 'hidden'
         })
 }
 
 const createCard = a => {
     document.querySelector('#title').innerHTML = a.activity
-    document.querySelector('#participants').innerHTML = a.participants
-    document.querySelector('#acc').innerHTML = a.accessibility
-    document.querySelector('#price').innerHTML = a.price
-    document.querySelector('#cat').innerHTML = a.type
+    document.querySelector('#participants').innerHTML = 'Participants: ' + a.participants
+    document.querySelector('#acc').innerHTML = 'Accessibility: ' + a.accessibility
+    document.querySelector('#price').innerHTML = 'Price: ' + a.price
+    document.querySelector('#cat').innerHTML ='Type: ' + a.type
 }
 getActivity()
 document.querySelector('#fetch').addEventListener('click',getActivity)
