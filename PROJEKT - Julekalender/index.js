32c60523e9aff145e9bde4d5f958a29ca9e4856c
Betak0123 <bjarke2003@gmail.com>
2021-12-07 14:11:20 +0100

    KALENDERPROJEKT - ARBEJDE PÅ SKOLEN

--- PROJEKT - Julekalender/index.js
+++ PROJEKT - Julekalender/index.js
@@ -4,10 +4,10 @@ let body = document.querySelector('body')
 let main = document.querySelector('main')
 let billede = document.querySelector('.billede')
 
-door.style.transform = 'rotateY(180deg)'
+// door.style.transform = 'rotateY(180deg)'
 
 
-const rotate = a =>{
+const rotate = () =>{
 
     if (door.style.transform == 'rotateY(180deg)'){
         document.querySelector('#doorsound').play()
@@ -19,8 +19,9 @@ const rotate = a =>{
     console.log(door.style.transform)
 }
 
-const expand = a =>{
-
+const expand = () =>{
+// doorway.style.position = 'absolute'
+// doorway.style.left = '50vw'    
 doorway.style.height = '100vh'
 doorway.style.width = '100vw'
 billede.style.height = '100vh'
@@ -28,29 +29,84 @@ billede.style.width = '100vw'
     console.log('hello world')
 }
 
-const låger = () => {
+
+fetch('./kalender.json')
+    .then(res => res.json())
+    .then(json => {
+        console.log(json) 
+        json.låger.map(låge =>{
+            låger(låge)    
+        })
+})
+const låger = (låge) => {
     let newContain = document.createElement('div')
+    let WholeDoor = document.createElement('div')
     let newDoorgrp = document.createElement('div')
     let newDoorway = document.createElement('div')
     let newDoor = document.createElement('div')
-    let link = document.createElement('a')
+    let billede = document.createElement('img')
+    let lågenummer = document.createElement('div')
+
+
+    billede.classList.add('billede')
     newContain.classList.add('container')
     newDoorgrp.classList.add('doorgroup')
     newDoorway.classList.add('doorway')
     newDoor.classList.add('door')
+    lågenummer.classList.add('lågenummer')
+
+    console.log(billede.classList.value )
+    billede.src = "./assets/julemand.gif"
+    lågenummer.innerHTML = låge.nummer
+    newDoor.style.transform = 'rotateY(180deg)'
+
+    newDoor.addEventListener('click', rot = () => {
+        if (newDoor.style.transform == 'rotateY(180deg)'){
+            document.querySelector('#doorsound').play()
+            newDoor.style.transform = 'rotateY(340deg)'  
+        }else{  
+            newDoor.style.transform = 'rotateY(180deg)'  
+            document.querySelector('#doorclose').play()
+        }
+        console.log(door.style.transform)
+    })
+
+    newDoor.append(lågenummer)
     newContain.append(newDoorgrp)
     newDoorgrp.append(newDoor, newDoorway)
-
+    WholeDoor.append(newDoorgrp)
+    main.append(WholeDoor)
 }
-{/* >
-    <div class="doorgroup">
-            <div class="doorway">Klik her</div>
-        <div class="door">
-            <img class= nummer src="./assets/nummer1.svg" alt="">
-            <audio id="doorsound"src="./assets/open_door.mp3"></audio>
-            <audio id="doorclose"src="./assets/doorclose.wav"></audio>
-        </div>
+
+{/* <div class="doorgroup">
+    <div class="doorway">
+        <img class='billede' src="./assets/julemand.gif" alt="">
     </div>
-*/}
-door.addEventListener('click',rotate)
-doorway.addEventListener('click',expand)
+    <div class="door">
+        <div class="lågenummer">q</div>
+        <audio id="doorsound"src="./assets/open_door.mp3"></audio>
+        <audio id="doorclose"src="./assets/doorclose.wav"></audio>
+    </div>
+</div> */}
+
+// const newCard = (låge) => {
+//     let card = document.createElement('div')
+//     let heading = document.createElement('h2')
+//     let members = document.createElement('div')
+//     let lågenumer = document.createElement('div')
+//     card.classList.add('card')
+//     members.classList.add('members')
+//     card.append(heading)
+//     card.append(members)
+//     document.querySelector('main').append(card)
+//     heading.innerHTML = låge.family
+//     let list = ''
+//     låge.members.map(member => list += '<li>' + member + '</li>')
+//     members.innerHTML = list
+//     document.querySelector('main').append(card)
+// }
+
+// document.querySelector('#door2').addEventListener('click',rotate)
+// document.querySelector('#door2').style.transform = 'rotateY(180deg)'
+// door.addEventListener('click',rotate)
+// doorway.addEventListener('click',expand)
