c40420feb31c92bd32f8981ca4be215820c863e8
Betak0123 <bjarke2003@gmail.com>
2022-01-12 18:42:22 +0100

    Hjemmearbejde

--- 111 - P5 - TEMPLATE/index.js
+++ 111 - P5 - TEMPLATE/index.js
@@ -1,23 +1,21 @@
 function setup(){
-    console.log('Hello world!')
-    frameRate(3600)
+    frameRate(60)
     createCanvas(windowWidth, windowHeight)
     background('black')    
-    select('#info').html('skriv her!')
 }
 
 function draw(){
     //mouseX mouseY frameCount map
 }
 
-function mousePressed(){
-    select('#info').html('Hovsa! uwu')
-}
+// function mousePressed(){
+//     select('#info').html('Hovsa! uwu')
+// }
 
-function mouseReleased(){
-    select('#info').html('skriv her!')
-}
+// function mouseReleased(){
+//     select('#info').html('skriv her!')
+// }
 
-function keyPressed(event){
-    select('#info').html('Du trykkede: ' + event.key)
-}
+// function keyPressed(event){
+//     select('#info').html('Du trykkede: ' + event.key)
+// }
---
+++ 113 - P5 - Bold/index.js
@@ -0,0 +1,95 @@
+let x, y, diameter
+let F_res
+let F_t = 9.82
+let F_luft = 0
+let velocity = 0
+let dt =    0.01
+let kLuft = 1
+let nyFrame = 0
+
+
+function setup(){
+    frameRate(60)
+    createCanvas(windowWidth, windowHeight)
+    background('black') 
+    x = windowWidth/2
+    y = 40
+    diameter = 60
+    y = diameter/2
+    show()
+    fill('hotpink')
+    stroke('white')
+    stroke
+}
+
+    new p5.Vector()
+function show (){
+    ellipse(x, y, diameter)
+}
+function update (){
+    F_luft = -0.04 * kLuft * velocity  
+
+    F_res = F_t + F_luft
+    velocity = velocity + F_res
+    velocity = velocity 
+    y += velocity
+        if(y>windowHeight-diameter/2){
+            y=windowHeight-diameter/2
+            velocity = -velocity
+        }
+    select('#info1').html('velocity:'+ round(velocity,1))
+    select('#info2').html(' F_res:' + round(F_res,1))
+    select('#info3').html(' F_luft: ' + round(F_luft, 2))
+}
+
+function restart(){
+    y = 40
+    F_t = 9.82
+    F_luft = 0
+    velocity = 0
+}
+
+function restartLuft(){
+    kLuft = document.querySelector('#inputLuft').value
+    select('#infoTop').html('Luftmodstand sat til: ' +document.querySelector('#inputLuft').value )
+    if(document.querySelector('#inputLuft').value == 0){
+        select('#infoTop').html('Luftmodstand sat til: 0')
+        setTimeout(() => {
+            document.querySelector('#infoTop').classList.add('gone')
+            // console.log('Det virkede')
+            setTimeout(() => {
+                document.querySelector('#infoTop').classList.remove('gone')
+                select('#infoTop').html('')
+                // console.log('Det virkede super!')
+        
+            }, 1000);
+        }, 1500);
+    }
+}
+function restartFrames(){
+    frameRate(document.querySelector('#inputFrames').value / 100 * 60)
+    select('#infoTop').html('Framerate sat til: ' + document.querySelector('#inputFrames').value / 100 * 60)
+    setTimeout(() => {
+        document.querySelector('#infoTop').classList.add('gone')
+        // console.log('Det virkede')
+        setTimeout(() => {
+            document.querySelector('#infoTop').classList.remove('gone')
+            select('#infoTop').html('')
+            // console.log('Det virkede super!')
+    
+        }, 1000);
+    }, 1500);
+}
+
+function draw(){
+    background('black')
+    show()
+    update()
+    rect(.04*windowWidth, .9 * windowHeight, 17, -20*F_res)
+    rect(.71*windowWidth, .9 * windowHeight, 17, 20*F_luft)
+    rect(.93*windowWidth, .9 * windowHeight, 17, -3*velocity)
+
+    // console.log(F_res)
+
+}
+
