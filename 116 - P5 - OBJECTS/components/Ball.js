1c702ce3c5fad31d20313586f5b30ec9f5ba0643
Betak0123 <bjarke2003@gmail.com>
2022-02-04 15:13:51 +0100

    CUM

--- 116 - P5 - OBJECTS/components/Ball.js
+++ 116 - P5 - OBJECTS/components/Ball.js
@@ -1,11 +1,12 @@
 console.log('suck a dick')
 
 class Bold{
-    constructor(radius, xpos, ypos, col, kLuft, name){
+    constructor(radius, xpos, ypos, col, kLuft, name, xvelocity){
         this.radius = radius
         this.xpos = xpos
         this.ypos = ypos
         this.col = col
+        this.xvelocity = xvelocity
 
         this.reset = ypos
 
@@ -14,7 +15,7 @@ class Bold{
         this.F_res = 0
         this.F_t = 9.82
         this.F_luft = 0
-        this.velocity = 0
+        this.velocity = random(-50,0)
         this.dt =    0.01
         this.kLuft = kLuft
     }
@@ -36,15 +37,35 @@ class Bold{
 
         this.F_res = this.F_t + this.F_luft
         this.velocity = this.velocity + this.F_res
-        this.velocity = this.velocity 
+        this.xvelocity = this.xvelocity*0.99     
         this.ypos += this.velocity
+        this.xpos += this.xvelocity
+        // console.log(this.xpos)
             if(this.ypos>height-this.radius/2){
                 this.ypos=height-this.radius/2
                 this.velocity = this.velocity*-1
             }
-            console.log(this.F_res)
+            if(this.xpos>width-this.radius/2){
+                // console.log('over')
+                this.xvelocity = this.xvelocity*-1
+            }
+            if(this.xpos<this.radius/2){
+                // console.log('under')
+                this.xvelocity = this.xvelocity*-1
+            }
+            // console.log(this.F_res)
+
             if(mouseIsPressed == true){
                 this.ypos = this.reset
+                this.F_t = 9.82
+                this.F_luft = 0
+                this.velocity = 0
+                this.radius = random(10,40)
+                this.col = color(random(255), random(255), random(255))
+                this.kLuft = random(0.001,2)
+                this.xvelocity = random(-30,30)
+
+            // console.log('duck')
             }
     }
 }
--- 116 - P5 - OBJECTS/index.js
+++ 116 - P5 - OBJECTS/index.js
@@ -1,26 +1,63 @@
 
 
-students = ['mie','bjarke','sofus','marius','bjørn','viktor','theis','frederik']
+// students = ['Mie','Bjarke','Sofus','Marius','Bjørn','Viktor','Theis','Frederik']
+students = ['pre']
 let balls = []
-
+let cumcontainer, cumcolor, img
+let cockblock = false
+let dick1, dick2
 
 function setup(){ 
-    createCanvas(windowWidth, 400)
+    createCanvas(windowWidth, 500)
     background('hotpink')
+    frameRate(60)
     students.map( (item,index) =>{
         let c = color(random(255), random(255), random(255))
-        let b = new Bold(random(10,40), index*45+10 , 200 , c,random(0.3,2),item)
+        let b = new Bold(random(10,40), index*45+15 , 200 , c,random(0.001,2),item,random(-50,50))
         balls.push(b)
-    })
-
-}
-
-function draw(){
-    background('hotpink')
-    balls.map( item =>{
-        item.show()
-        item.update()
+        students.push(index)
+        img = loadImage('./assets/kiks.png')
+        dick1 = loadImage('./assets/dick.png')
+        dick2 = loadImage('./assets/dick.png')
     })
     
 }
 
+function draw(){
+    background('hotpink')
+    image(dick1, windowWidth/2-windowWidth/4-50, 170, 100, 150)
+    image(dick2, windowWidth/2+windowWidth/4-50, 170, 100, 150)
+    image(img, windowWidth/2-100, 350, 200, 200)
+    
+    if(cockblock==false){
+        if(frameCount%1 == 0){
+            let b = new Bold(random(10,40), windowWidth/2-windowWidth/4 , 200 , 250,random(0.001,2),'cum  ',random(-50,50))
+            let c = new Bold(random(10,40), windowWidth/2+windowWidth/4 , 200 , 250,random(0.001,2),'cum  ',random(-50,50))
+
+            balls.push(b,c)
+        }
+        
+    }
+    if(frameCount%2 == 0){
+        
+        balls.map( item =>{
+            item.update()  
+        })
+    }
+    balls.map( item =>{
+        item.show()
+    })
+    select('#counter').html('Antal cum:' + balls.length)
+    cumcontainer = map(balls.length, 0, 100, 10, windowWidth-20, true)
+    greencum = map(balls.length, 0, 100, 255, 0, true)
+    redcum = map(balls.length, 0, 100, 0, 255, true)
+    fill(redcum,greencum,0)
+    rect(10, 10 , cumcontainer, 40)
+    // console.log(cumcontainer)
+    
+    if(balls.length>99){
+    select('#cumOverload').html('!!!CUM OVERLOAD!!!')
+    console
+    cockblock = true
+}
+}
