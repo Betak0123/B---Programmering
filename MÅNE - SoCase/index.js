469ad3eb49b3931ef2abd734e0ee0aebb2c36040
Betak0123 <bjarke2003@gmail.com>
2022-03-02 10:49:34 +0100

    Mere arbejde med Firebases

---
+++ MÅNE - SoCase/index.js
@@ -0,0 +1,29 @@
+function setup(){
+    frameRate(60)
+    createCanvas(windowWidth, windowHeight)
+    background('black')    
+}
+
+function draw(){
+
+}
+
+
+
+
+
+
+
+
+ //mouseX mouseY frameCount map
+// function mousePressed(){
+//     select('#info').html('Hovsa! uwu')
+// }
+
+// function mouseReleased(){
+//     select('#info').html('skriv her!')
+// }
+
+// function keyPressed(event){
+//     select('#info').html('Du trykkede: ' + event.key)
+// }
